fcc5f7a73773bf1cd982fe22d19991c3997b4b6d
FranciscoHT <franz140000@gmail.com>
2024-10-03 17:02:01 -0500

    Initial commit

---
+++ bookings-service/src/main/java/example/microservices/bookingsservice/BookingsServiceApplication.java
@@ -0,0 +1,13 @@
+package example.microservices.bookingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingsServiceApplication.class, args);
+    }
+
+}
